88104a85a395dfda9b88de5cbdf1e42aeba4d1ba
Guido Paliot <guido@paliot.de>
2013-09-18 16:51:04 +0200

    Comments and formatting

--- fwlogcomb.pl
+++ fwlogcomb.pl
@@ -9,6 +9,7 @@ use Getopt::Long;
 
 use Data::Dumper;
 
+# Globals
 my %net;
 my $ip;
 my $cidr;
@@ -19,12 +20,16 @@ my $whois;
 my $state_file;
 my $whois_timeout = 15;
 
+
+
 sub load_state {
     my %state;
     my $filename = shift;
     my $cdr;
+    # Load the state if the file exists
     if (-e $filename) {
         %state = %{retrieve $filename};
+        # Remove all counters and sums
         foreach $cdr (keys (%state)) {
             undef $state{$cdr}{'sum'};
             undef $state{$cdr}{'addresses'};
@@ -39,38 +44,57 @@ sub save_state {
     store $state, $filename;
 }
 
+# Read command line options
 GetOptions (
     'state|s:s' => \$state_file,
     'whois-timeout|t:i' => \$whois_timeout
 );
 
+# Load the state (whois cache) if the statefile option was set
 %net = load_state($state_file) if ($state_file);
 
+# Read IPs from STDIN
 while ($ip = <>) {
+    # Remove newline
     chomp $ip;
+
+    # Check if the IP is valid IPv4 or IPv6
     if (cidrvalidate($ip)) {
+        # Assume a new net
         $cidr_is_new = 1;
+
+        # Check if the IP matches one of the known nets
         foreach $cidr (keys (%net)) {
+            # On match, add the IP occurence and mark the net as known
             if (cidrlookup($ip, $cidr)) {
                 $net{$cidr}{'addresses'}{$ip}++;
                 $net{$cidr}{'sum'}++;
                 $cidr_is_new = 0;
             }
         }
+        # For new nets
         if ($cidr_is_new) {
+            # Lookup whois info online, could be replaced by offline DB
             $whois = whoisip_query($ip) || die "WHOIS error";
             sleep $whois_timeout;
+
+            # Calculate the CIDR from the whois inetnum and add whois info
             foreach $cidr (range2cidr($whois->{'inetnum'})) {
                 foreach (keys(%{$whois}) ) { $net{$cidr}{'whois'}{$_} = $whois->{$_} };
+                    # Add the IP occurence
                     $net{$cidr}{'addresses'}{$ip}++;
                     $net{$cidr}{'sum'}++;
+
+                    # Set the new CIDR state to unknown
                     $net{$cidr}{'state'} = 'unknown';
             }
         }
     }
 }
 
+# Save state if the statefile option was set
 save_state(\%net, $state_file) if ($state_file);
 
+# Dump the hash in lieu of real output
 print Dumper(\%net);
 
